80c5916cd2f2d2bae7d448b9b18497d7eb6a2430
bibaswan <bibaswan.pattjoshi@zippr.in>
2018-08-16 14:57:24 +0530

    Added manifest and added bbound method

--- app/src/main/java/com/example/bibaswan/samplebbound/MainActivity.java
+++ app/src/main/java/com/example/bibaswan/samplebbound/MainActivity.java
@@ -2,17 +2,46 @@ package com.example.bibaswan.samplebbound;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import com.bebound.sdk.BeBound;
+import com.bebound.sdk.authentication.AuthManager;
+import com.bebound.sdk.authentication.listener.AuthListener;
+
+public class MainActivity extends AppCompatActivity implements AuthListener {
+
+    private static final String TAG = "Main Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (!BeBound.isUserAuthenticated()) {
+            new Thread(() -> AuthManager.getInstance()
+                    .withContext(this)
+                    .onAuthListener(this)
+                    .send())
+                    .start();
+        }
     }
 
     public void onSendRequestClick(View view){
 
+    }
+
+    @Override
+    public void onAuthenticationSent() {
+        Log.d(TAG, "Authentication request sent");
+    }
+
+    @Override
+    public void onUserAuthenticated() {
+        Log.d(TAG, "Success! User is authenticated");
+    }
+
+    @Override
+    public void onErrorDuringUserAuthentication(int errorStatus) {
+        Log.d(TAG, "Error during authentication!");
     }
 }
